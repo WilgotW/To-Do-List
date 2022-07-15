e5a8505af25dda328ee9d1c5302cdf7f90d8c7c0
Wilgot <wilgotwardell@hotmail.com>
2022-07-15 18:37:48 +0200

    better colors and grid

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const colorOptions = [
     "#f2b880", //peach
     "#fff4ec", //white
 ];
+let colorIndex = 0;
 
 const addBtn = document.getElementById("addButton");
 const taskBlock = document.getElementById("taskBlock");
@@ -15,25 +16,27 @@ let taskInput;
 let taskText;
 
 addBtn.addEventListener('click', function addClicked(event){
-    
     tasks.push(document.createElement('div'));
-
     taskInput = document.getElementById("taskInput");
 
     tasks[taskIndex].textContent = taskInput.value;
-    let color = colorOptions[generateColor()];
+    taskInput.value = "";
+
+    let color = colorOptions[colorIndex];
+    colorIndex++;
+    if(colorIndex > colorOptions.length -1){
+        colorIndex = 0;
+    }
     tasks[taskIndex].style.backgroundColor = color;
-
-    tasks[taskIndex].style.width = '150px';
-    tasks[taskIndex].style.height = '150px';
-
+    tasks[taskIndex].className = "task"
+    
     taskBlock.appendChild(tasks[taskIndex]);
     taskIndex++;
 });
 
-function generateColor(){
-    return Math.floor(Math.random() * (3 - 0 + 1) + 0);
-}
+// function generateColor(){
+//     return Math.floor(Math.random() * (3 - 0 + 1) + 0);
+// }
 
 
 
